469222f92fbb7e683a8f11ca6c40e4b4a333b2d2
Andrew Izatt <izatt.andrew@gmail.com>
2025-01-28 10:15:37 -0700

    complete assignment

--- task/task.py
+++ task/task.py
@@ -1,1 +1,4 @@
-# Write your code below this line 👇
+print("Welcome to your personal band name generator!")
+city = input("What city did you grow up in?\n")
+pet = input("What was the name of your first pet?\n")
+print("Your band name is " + city + " " + pet + "!")
